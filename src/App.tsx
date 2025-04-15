da0b358312ddc1372945e476a52c33ec283a95b2
Aneesh Sharma <aneeshsharma0671@gmail.com>
2025-04-15 12:32:34 +0530

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
-    <>
+    <div style={{ backgroundColor: 'black', color: 'white', width: '100%', height: '100%' }}>
       <Navbar />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,11 +5,12 @@ const Navbar: React.FC = () => {
   return (
     <header className={styles.navbar}>
       <div className={styles.logo}>
-        <a href="">Aneesh Sharma</a>
+        <a className={styles.logoLink} href="/">Aneesh Sharma</a>
       </div>
-      <nav className={styles.navLinks}>
-        <a href="">About</a>
-        <a href="">Projects</a>
+      <nav className={styles.menu}>
+        <a className={styles.menuItem} href="#about">About</a>
+        <a className={styles.menuItem} href="#projects">Projects</a>
+        <a className={styles.menuItem} href="#contact">Contact</a>
       </nav>
     </header>
   );
