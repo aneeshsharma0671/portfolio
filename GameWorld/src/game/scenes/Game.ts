5cb307ad75f6240fb4a3951e8753f2c175b065f5
Aneesh Sharma <aneeshsharma0671@gmail.com>
2025-07-03 13:02:06 +0530

    Added Phaser Scene

---
+++ GameWorld/src/game/scenes/Game.ts
@@ -0,0 +1,22 @@
+import { Scene } from "phaser";
+
+export class Game extends Scene {
+  camera: Phaser.Cameras.Scene2D.Camera;
+  background: Phaser.GameObjects.Image;
+
+  constructor() {
+    super("Game");
+  }
+
+  create() {
+    this.camera = this.cameras.main;
+    this.camera.setBackgroundColor(0x00ff00);
+
+    this.background = this.add.image(512, 384, "background");
+    this.background.setAlpha(0.5);
+
+    this.input.once("pointerdown", () => {
+      this.scene.start("GameOver");
+    });
+  }
+}
