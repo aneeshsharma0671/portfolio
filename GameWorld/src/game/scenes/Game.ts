3e4df179a7712fb65a8df9e2dc9b0803ee8133b5
Aneesh Sharma <aneeshsharma0671@gmail.com>
2025-07-03 14:05:06 +0530

    Added Sample Tile Map

--- GameWorld/src/game/scenes/Game.ts
+++ GameWorld/src/game/scenes/Game.ts
@@ -8,12 +8,27 @@ export class Game extends Scene {
     super("Game");
   }
 
+  preload() {
+    // Load assets for the game scene
+    this.load.image("tiles", "assets/tilemap.png");
+    this.load.tilemapCSV("map", "assets/Tile.csv");
+  }
+
   create() {
     this.camera = this.cameras.main;
     this.camera.setBackgroundColor(0x00ff00);
 
     this.background = this.add.image(512, 384, "background");
     this.background.setAlpha(0.5);
+
+    const map = this.make.tilemap({
+      key: "map",
+      tileWidth: 16,
+      tileHeight: 16,
+    });
+    const tileset = map.addTilesetImage("tiles");
+    const layer = map.createLayer(0, tileset!, 0, 0); // layer index, tileset, x, y
+    layer!.skipCull = true;
 
     this.input.once("pointerdown", () => {
       this.scene.start("GameOver");
