7a1b86014d43c7a0bb69509a2bc0ff9405be3300
Aneesh Sharma <aneeshsharma0671@gmail.com>
2025-09-10 12:45:38 +0530

    converted pages to typescript

--- src/Pages/about.tsx
+++ src/Pages/about.tsx
@@ -6,8 +6,8 @@ export class about extends Component {
     return (
         <div className='Main-Div'>
           <div className='header'>About Me</div>
-            <div class="social-icons">
-              <a class="item fa fa-file-text-o" href='https://drive.google.com/file/d/1Zt3iNiLZVnSzQOrWsQv5lQB2DICa5tLw/view?usp=sharing'>Resume</a>
+            <div className="social-icons">
+              <a className="item fa fa-file-text-o" href='https://drive.google.com/file/d/1Zt3iNiLZVnSzQOrWsQv5lQB2DICa5tLw/view?usp=sharing'>Resume</a>
             </div>
         </div>
     )
---
+++ src/Pages/contact.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './pages.css'
+export class contact extends Component {
+  render() {
+    return (
+      <div className='Main-Div'>
+        <div className='header'>Contact Me</div>
+        <div className="social-icons">
+          <a className="item fa fa-envelope" href='mailto:aneeshsharma0671@gmail.com'>Mail-To</a>
+          <a className="item fa fa-linkedin" href="https://www.linkedin.com/in/aneeshsharma0671/">linkedin</a>
+          <a className="item fa fa-github" href="https://github.com/aneeshsharma0671">Github</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default contact
