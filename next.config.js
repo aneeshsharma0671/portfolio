95308c62a529118be797b426cf9fb9cad7343698
Aneesh Sharma <54682356+aneeshsharma0671@users.noreply.github.com>
2026-01-14 15:26:45 +0000

    Dockerize Next.js app

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // For static export, since it's a portfolio
-  trailingSlash: true,
-  images: { unoptimized: true }, // If using images
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
